8308f62b74c87516be0bf513c979bbe71d5783f9
Lord-Meca <lordmeca3@gmail.com>
2022-10-23 10:18:53 +0200

    c7

--- js/mainHandler.js
+++ js/mainHandler.js
@@ -16,15 +16,15 @@ function copyDiscord(){
 function checkBoxNavBar(){
 
     const barsButton = document.querySelector("#check");
-    const footer = document.querySelector(".footer");
+  
     const middleTextDiv = document.querySelector(".middle-text");
 
     if(barsButton.checked) {
-        footer.style.display = "none";
+ 
         middleTextDiv.style.display = "none";
         document.getElementById("icon").className = "fa fa-close";
     } else {
-        footer.style.display = "grid";
+        
         middleTextDiv.style.display = "block";
         document.getElementById("icon").className = "fa fa-bars";
     }
@@ -53,9 +53,12 @@ setInterval(() =>{
             document.querySelector(".button").style.padding = "12px 40px";
             document.querySelector(".middle-text").style.paddingTop = "200px";
             document.querySelector(".text").style.fontSize = "24px";
-
-            document.querySelector(".footer").style.display = "grid";
-            document.querySelector(".button").style.marginTop = "0";
+            
+            document.querySelector(".button").style.marginTop = "-2%";
+            
+        
+        } else {
+    
         }
     }
 
@@ -63,19 +66,20 @@ setInterval(() =>{
 
     
 
-        document.querySelector(".button").style.fontSize = "40%";
+        document.querySelector(".button").style.fontSize = "50%";
         document.querySelector(".middle-text").style.paddingTop = "100px";
         document.querySelector(".text").style.fontSize = "12px";
-        document.querySelector(".button").style.marginTop = "0";
+        document.querySelector(".button").style.marginTop = "-5%";
+
 
     } 
 
     if(document.body.offsetWidth <= 612){
 
         if(document.body.offsetHeight <= 619){
-            document.querySelector(".button").style.fontSize = "20%";
+            document.querySelector(".button").style.fontSize = "40%";
             document.querySelector(".button").style.padding = "8px 30px";
-            document.querySelector(".button").style.marginTop = "-3%";
+            document.querySelector(".button").style.marginTop = "-10%";
             document.querySelector(".middle-text").style.paddingTop = "60px";
             document.querySelector(".text").style.fontSize = "7px";
         } else {
@@ -91,15 +95,18 @@ setInterval(() =>{
         document.querySelector(".button").style.padding = "12px 40px";
         document.querySelector(".middle-text").style.paddingTop = "200px";
         document.querySelector(".text").style.fontSize = "24px";
+        
     
     }
 
     if(document.body.offsetWidth < 350){
         document.querySelector(".middle-text").style.display = "none";
+       
     } else {
 
         if(!barsButton.checked){
             document.querySelector(".middle-text").style.display = "block";
+          
         }
 
         
